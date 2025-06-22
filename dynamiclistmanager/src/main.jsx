44dc3ef1fdef9b646fa7b2c0b2a50d55c37cc8ea
Hillary Mwangi Maina <hillarymwangimaina@gmail.com>
2025-06-22 13:30:20 +0300

    Update main.jsx

--- dynamiclistmanager/src/main.jsx
+++ dynamiclistmanager/src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+import DynamicListManager from './DynamicListManager'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<DynamicListManager />)
